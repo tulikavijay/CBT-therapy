9920830a09f53e17feb78255f4ba0f069f023074
tux <fictionfree54@gmail.com>
2018-02-09 22:43:12 +0530

    Add depression screening test

--- CBT_therapy/settings.py
+++ CBT_therapy/settings.py
@@ -121,4 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
