022d636aa115f8fbe9e09ae8784260f3c6a47b87
tux <fictionfree54@gmail.com>
2018-03-04 23:43:41 +0530

    Add allowed host to settings

--- CBT_therapy/settings.py
+++ CBT_therapy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tym7-hn#nzaoovw-_f#czwy)e9&u)9i0yrm#^^l7xcmgl#qpv@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['nameless-island-79297.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
