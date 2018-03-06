614a23d8433c9b80c280ef2d4d7576be9fad6427
Tulika <fictionfree54@gmail.com>
2018-03-06 23:09:38 +0530

    Update settings.py

--- CBT_therapy/settings.py
+++ CBT_therapy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tym7-hn#nzaoovw-_f#czwy)e9&u)9i0yrm#^^l7xcmgl#qpv@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nameless-island-79297.herokuapp.com','127.0.0.1','localhost']
 
@@ -141,12 +141,12 @@ FIXTURE_DIRS=[
 ]
 
 # Production settings
-SECURE_HSTS_SECONDS = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_PRELOAD = True
+# SECURE_HSTS_SECONDS = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+# SECURE_CONTENT_TYPE_NOSNIFF = True
+# SECURE_BROWSER_XSS_FILTER = True
+# SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# X_FRAME_OPTIONS = 'DENY'
+# SECURE_HSTS_PRELOAD = True
