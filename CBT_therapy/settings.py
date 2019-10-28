e8145cff923bdbdddf08bfa98f33f518dc05de44
Nicole Ortiz <e096605@nisd.net>
2019-10-28 09:23:30 -0500

    Added bootstrap 4 in settings.py and general UI changes
    Added Bootstrap 4 in settings.py to render forms using Bootstrap 4 CSS classes.

--- CBT_therapy/settings.py
+++ CBT_therapy/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
